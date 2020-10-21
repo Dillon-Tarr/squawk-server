b7ea6eaea4b3a5c32f6f024a3005e627371f1b96
Dillon Tarr <56458494+Dillon-Tarr@users.noreply.github.com>
2020-10-20 22:53:25 -0400

    began work on new user post route

--- models/user-Schema.js
+++ models/user-Schema.js
@@ -4,7 +4,7 @@ const Joi = require('joi');
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true, minlength: 6, maxlength: 18 },
   password: { type: String, required: true, unique: false, minlength: 8, maxlength: 30 },
-  emailAddress: { type: String, required: true, unique: true, minlength: 6, maxlength: 128 },
+  emailAddress: { type: String, required: true, unique: true, minlength: 6, maxlength: 255 },
 //profilePicture: { type: Buffer??, required: true, unique: false},
   aboutMe: { type: String, required: true, unique: false, minlength: 0, maxlength: 1024 },
   birdCall: { type: String, required: true, unique: false, minlength: 0, maxlength: 30 },
@@ -18,12 +18,12 @@ const User = mongoose.model('User', userSchema);
 
 function validateUser(user) {
   const schema = Joi.object({
-  username: Joi.string().required(),
-  password: Joi.string().required(),
-  emailAddress: Joi.string().required(),
+  username: Joi.string().required().min(6).max(18),
+  password: Joi.string().required().min(8).max(30),
+  emailAddress: Joi.string().required().email().min(6).max(255),
 //profilePicture: 
-  aboutMe: Joi.string().required(),
-  birdCall: Joi.string().required(),
+  aboutMe: Joi.string().required().min(0).max(1024),
+  birdCall: Joi.string().required().min(0).max(30),
   myBirds: Joi.array().required(),
   birdsIWatch: Joi.array().required(),
   friends: Joi.array().required()
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,30 @@ const {User, validateUser} = require('../models/user-Schema');
 const express = require('express');
 const router = express.Router();
 
-
+router.post('/', async (req, res) => {
+  try {
+  const { error } = validateUser(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  let user = await User.findOne({ username: req.body.username });
+  if (user) return res.status(400).send('User with that username is already registered.');
+  user = await User.findOne({ emailAddress: req.body.emailAddress });
+  if (user) return res.status(400).send('User with that email address is already registered.');
+  user = new User({
+  username: req.body.username,
+  password: req.body.password,
+  emailAddress: req.body.emailAddress,
+  aboutMe: "",
+  birdCall: "",
+  myBirds: [],
+  birdsIWatch: [],
+  friends: []
+  });
+  await user.save();
+  return res.send({ username: user.username, emailAddress: user.emailAddress });
+  } catch (ex) {
+  return res.status(500).send(`Internal Server Error: ${ex}`);
+  }
+ });
+ 
 
 module.exports = router;
